c32f23e2011695d4f52e60aa3b48051f3134d459
Coco <crazypad@qq.com>
2019-02-28 12:47:48 +0800

    support export-declaration

--- dist/lib/converter/nodes/export.d.ts
+++ dist/lib/converter/nodes/export.d.ts
@@ -4,5 +4,7 @@ import { Context } from '../context';
 import { ConverterNodeComponent } from '../components';
 export declare class ExportConverter extends ConverterNodeComponent<ts.ExportAssignment> {
     supports: ts.SyntaxKind[];
-    convert(context: Context, node: ts.ExportAssignment): Reflection;
+    private _convertExportAllDeclaration;
+    private _convertExportDeclaration;
+    convert(context: Context, node: ts.ExportAssignment | ts.ExportDeclaration): Reflection | undefined;
 }
