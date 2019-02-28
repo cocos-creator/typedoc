7bb47f4dc193030d24f9b178f990a1b658f99f73
Coco <crazypad@qq.com>
2019-02-28 12:42:49 +0800

    update own repository

---
+++ dist/test/module/a.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const b_1 = require("./b");
+function add(a, b) {
+    return a + b_1.multiply(b, 1);
+}
+exports.add = add;
+//# sourceMappingURL=a.js.map
---
+++ dist/test/module/b.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiply(a, b) {
+    return a * b;
+}
+exports.multiply = multiply;
+//# sourceMappingURL=b.js.map
